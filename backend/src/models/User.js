57b947eb1972394907e73b5be37946569a413839
SupermaxiMarket <monsunrise@gmail.com>
2025-03-01 11:01:38 +0100

    Ajout du modèle User

---
+++ backend/src/models/User.js
@@ -0,0 +1,72 @@
+// backend/src/models/User.js
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+const bcrypt = require('bcrypt');
+
+const User = sequelize.define('User', {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true
+    }
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  businessType: {
+    type: DataTypes.ENUM('Freelance', 'PME'),
+    allowNull: false
+  },
+  isPremium: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false
+  },
+  subscriptionStatus: {
+    type: DataTypes.ENUM('active', 'canceled', 'expired', 'none'),
+    defaultValue: 'none'
+  },
+  subscriptionEndDate: {
+    type: DataTypes.DATE,
+    allowNull: true
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW
+  },
+  updatedAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW
+  }
+}, {
+  hooks: {
+    beforeCreate: async (user) => {
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(user.password, salt);
+    },
+    beforeUpdate: async (user) => {
+      if (user.changed('password')) {
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(user.password, salt);
+      }
+    }
+  }
+});
+
+// Méthode pour vérifier le mot de passe
+User.prototype.verifyPassword = async function(password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+module.exports = User;
